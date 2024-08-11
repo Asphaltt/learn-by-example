24d135405b95247207bcd1e2a2e8a4fce688d5ba
Leon Hwang <hffilwlqm@gmail.com>
2024-08-11 21:29:17 +0800

    toa: 2 more ways
    Signed-off-by: Leon Hwang <hffilwlqm@gmail.com>

--- ebpf/toa/main.go
+++ ebpf/toa/main.go
@@ -12,13 +12,19 @@ import (
 
 	"internal/pkg/errx"
 
+	"github.com/cilium/ebpf"
 	"github.com/cilium/ebpf/link"
 	"github.com/cilium/ebpf/rlimit"
+	flag "github.com/spf13/pflag"
 )
 
 //go:generate go run github.com/cilium/ebpf/cmd/bpf2go -cc clang toa ./toa.c -- -D__TARGET_ARCH_x86 -I../headers -Wall
 
 func main() {
+	var way string
+	flag.StringVarP(&way, "way", "w", "1", "use toa way 1, 2 or 3")
+	flag.Parse()
+
 	errx.Check(rlimit.RemoveMemlock(), "Failed to remove rlimit memlock")
 
 	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
@@ -28,8 +34,22 @@ func main() {
 	errx.CheckVerifierErr(loadToaObjects(&objs, nil), "Failed to load toa objects")
 	defer objs.Close()
 
+	var prog *ebpf.Program
+	switch way {
+	case "1":
+		prog = objs.Toa1
+	case "2":
+		prog = objs.Toa2
+	case "3":
+		prog = objs.Toa3
+	case "4":
+		prog = objs.Toa4
+	default:
+		log.Fatalf("Invalid way: %s", way)
+	}
+
 	l, err := link.AttachTracing(link.TracingOptions{
-		Program: objs.FentryTcpV4SynRecvSock,
+		Program: prog,
 	})
 	errx.Check(err, "Failed to attach fexit(tcp_v4_syn_recv_sock)")
 	defer l.Close()
--- ebpf/toa/toa.c
+++ ebpf/toa/toa.c
@@ -25,23 +25,117 @@ struct toa_data {
 	__u32 ip;
 };
 
+static __always_inline bool
+__read_toa_4(struct tcphdr *tcp, struct toa_data *toa)
+{
+    void *buff;
+    int length;
+
+    length  = BPF_CORE_READ_BITFIELD_PROBED(tcp, doff) << 2;
+    length -= sizeof(struct tcphdr);
+
+    buff = (void *) tcp + sizeof(struct tcphdr);
+
+    while (length > 0) {
+        __u8 opcode, opsize;
+
+        /* bpf_probe_read_kernel() expects to read a toa option. If it fails to
+         * read it, it won't be a toa option; then return false because there's
+         * not enough data to read as a toa option.
+         */
+        if (bpf_probe_read_kernel(toa, sizeof(*toa), buff) < 0)
+            return false;
+
+        opcode = toa->opcode;
+        opsize = toa->opsize;
+
+        if (opcode == TCPOPT_EOL)
+            return false;
+        if (opcode == TCPOPT_NOP) { /* Ref: RFC 793 section 3.1 */
+            buff++;
+            length--;
+            continue;
+        }
+
+        if (opsize < 2)             /* "silly options" */
+            return false;
+        if (opsize > length)        /* don't parse partial options */
+            return false;
+
+        if (opcode == TCPOPT_TOA_COMPAT || opcode == TCPOPT_TOA_AKAMAI) {
+            if (opsize == TCPOLEN_TOA)
+                return true;
+        }
+
+        buff += opsize;
+        length -= opsize;
+    }
+
+    return false;
+}
+
+static __always_inline bool
+__read_toa_3(struct tcphdr *tcp, struct toa_data *toa)
+{
+    void *buff;
+    int length;
+
+    length  = BPF_CORE_READ_BITFIELD_PROBED(tcp, doff) << 2;
+    length -= sizeof(struct tcphdr);
+
+    buff = (void *) tcp + sizeof(struct tcphdr);
+
+    while (length > 0) {
+        __u8 opcode, opsize;
+
+        if (bpf_probe_read_kernel(&opcode, sizeof(opcode), buff) < 0)
+            return false;
+
+        if (opcode == TCPOPT_EOL)
+            return false;
+        if (opcode == TCPOPT_NOP) { /* Ref: RFC 793 section 3.1 */
+            buff++;
+            length--;
+            continue;
+        }
+
+        if (bpf_probe_read_kernel(&opsize, sizeof(opsize), buff + 1) < 0)
+            return false;
+
+        if (opsize < 2)             /* "silly options" */
+            return false;
+        if (opsize > length)        /* don't parse partial options */
+            return false;
+
+        if (opcode == TCPOPT_TOA_COMPAT || opcode == TCPOPT_TOA_AKAMAI) {
+            if (opsize == TCPOLEN_TOA) {
+                if (bpf_probe_read_kernel(toa, sizeof(*toa), buff-2) < 0)
+                    return false;
+                return true;
+            }
+        }
+
+        buff += opsize;
+        length -= opsize;
+    }
+
+    return false;
+}
+
 struct toa_prober {
     struct toa_data *toa;
     int length;
     int offset;
 };
 
-static __always_inline int
+static int
 __probe_toa(struct toa_prober *prober, void *buff)
 {
-    int ret;
-
     if (prober->offset + sizeof(*prober->toa) > prober->length)
         return -1;
 
-    ret = bpf_probe_read_kernel(prober->toa, sizeof(*prober->toa), buff + prober->offset);
-    if (ret)
-        return ret;
+    if (bpf_probe_read_kernel(prober->toa, sizeof(*prober->toa), buff + prober->offset) < 0)
+        return -1;
 
     if (prober->toa->opcode == TCPOPT_EOL)
         return -1;
@@ -94,7 +188,10 @@ __read_toa(struct tcphdr *tcp, struct toa_data *toa)
     length = BPF_CORE_READ_BITFIELD_PROBED(tcp, doff) << 2;
     length -= sizeof(struct tcphdr);
 
-    if (bpf_probe_read_kernel(buff, MAX_TCPOPT_LEN, (void *) tcp + sizeof(struct tcphdr)))
+    if (length <= 0)
+        return false;
+
+    if (bpf_probe_read_kernel(buff, MAX_TCPOPT_LEN, (void *) tcp + sizeof(struct tcphdr)) < 0)
         return false;
 
     for (int i = 0; i < MAX_TCPOPT_LEN - sizeof(*toa); ) {
@@ -147,18 +244,40 @@ __read_toa(struct tcphdr *tcp, struct toa_data *toa)
     return false;
 }
 
-SEC("fentry/tcp_v4_syn_recv_sock")
-int BPF_PROG(fentry__tcp_v4_syn_recv_sock, const struct sock *sk, struct sk_buff *skb)
+static int
+read_toa(struct sk_buff *skb, bool (* fn)(struct tcphdr *, struct toa_data *))
 {
     __u16 l4_off = BPF_CORE_READ(skb, transport_header);
     void *skb_head = BPF_CORE_READ(skb, head);
     struct toa_data toa;
 
-    if (!__read_toa_2(skb_head + l4_off, &toa) || !__read_toa(skb_head + l4_off, &toa))
-        return BPF_OK;
-
-    bpf_printk("TOA: opcode=%d, opsize=%d, port=%d, ip=%pI4\n",
-               toa.opcode, toa.opsize, bpf_ntohs(toa.port), &toa.ip);
+    if (fn(skb_head + l4_off, &toa))
+        bpf_printk("TOA: opcode=%d, opsize=%d, port=%d, ip=%pI4\n",
+                   toa.opcode, toa.opsize, bpf_ntohs(toa.port), &toa.ip);
 
     return BPF_OK;
+}
+
+SEC("fentry/tcp_v4_syn_recv_sock")
+int BPF_PROG(toa1, const struct sock *sk, struct sk_buff *skb)
+{
+    return read_toa(skb, __read_toa);
+}
+
+SEC("fentry/tcp_v4_syn_recv_sock")
+int BPF_PROG(toa2, const struct sock *sk, struct sk_buff *skb)
+{
+    return read_toa(skb, __read_toa_2);
+}
+
+SEC("fentry/tcp_v4_syn_recv_sock")
+int BPF_PROG(toa3, const struct sock *sk, struct sk_buff *skb)
+{
+    return read_toa(skb, __read_toa_3);
+}
+
+SEC("fentry/tcp_v4_syn_recv_sock")
+int BPF_PROG(toa4, const struct sock *sk, struct sk_buff *skb)
+{
+    return read_toa(skb, __read_toa_4);
 }
