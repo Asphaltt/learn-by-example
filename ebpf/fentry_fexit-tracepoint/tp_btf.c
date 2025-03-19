11134a55febdb0e172e478e40f386f74e5d2c823
Leon Hwang <leon.hwang@linux.dev>
2025-03-19 21:05:17 +0800

    update fentry_fexit-tracepoint
    Signed-off-by: Leon Hwang <leon.hwang@linux.dev>

--- ebpf/fentry_fexit-tracepoint/tp_btf.c
+++ ebpf/fentry_fexit-tracepoint/tp_btf.c
@@ -9,10 +9,8 @@
 #include "lib_tp_msg.h"
 
 SEC("tp_btf/netlink_extack")
-int BPF_PROG(tp__netlink_extack, __u32 _msg)
+int BPF_PROG(tp__netlink_extack, char *msg)
 {
-	char *msg = (void *)(__u64) ((void *) ctx + (__u64) ((_msg) & 0xFFFF));
-
 	__output_msg(ctx, msg, PROBE_TYPE_DEFAULT, 0);
 
 	return 0;
